b26b8b4981681a08acdaf2ccb6c84f9359eda819
DTM9025 <DTM9025@outlook.com>
2017-08-14 17:14:57 -0700

    [lang_en-shifted_bgm] Add patch to shift bgm titles to not cover up FPS counter

---
+++ lang_en-shifted_bgm/files.js
@@ -0,0 +1,17 @@
+{
+	"patch.js": 263015206,
+	"th15/front/logo/st01logo.png": 1919265330,
+	"th15/front/logo/st02logo.png": 3759234253,
+	"th15/front/logo/st03logo.png": 3922026960,
+	"th15/front/logo/st04logo.png": 749860241,
+	"th15/front/logo/st05logo.png": 760954703,
+	"th15/front/logo/st06logo.png": 3655368443,
+	"th15/front/logo/st07logo.png": 1663133822,
+	"th16/front/logo/st01logo.png": 279105129,
+	"th16/front/logo/st02logo.png": 1649944440,
+	"th16/front/logo/st03logo.png": 3185983032,
+	"th16/front/logo/st04logo.png": 937268691,
+	"th16/front/logo/st05logo.png": 4007836171,
+	"th16/front/logo/st06logo.png": 1916716794,
+	"th16/front/logo/st07logo.png": 1180236997
+}
---
+++ lang_en-shifted_bgm/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "lang_en-shifted_bgm",
+	"servers": [
+		"https://raw.githubusercontent.com/DTM9025/DTM/master/lang_en-shifted_bgm/"
+	],
+	"title": "English Language pack with shifted music titles in stage"
+}
