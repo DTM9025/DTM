822b191e4b7b9a11557ccf08f056f3c8a2805109
DTM9025 <DTM9025@outlook.com>
2017-08-14 17:18:45 -0700

    [lang_en-shifted_bgm] th14: Add th14 and updated repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "DTM9025@outlook.com",
 	"id": "DTM",
 	"patches": {
-		"alphes": "Alphes Styled Character Portraits (Art by Dairi)"
+		"alphes": "Alphes Styled Character Portraits (Art by Dairi)",
+		"lang_en-shifted_bgm": "English Language pack with shifted music titles in stage"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/DTM9025/DTM/master/"
