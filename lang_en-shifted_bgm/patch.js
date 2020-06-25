1dfddaccb400627bd56a6a0cb1168291d6a5c4ab
DTM9025 <DTM9025@outlook.com>
2020-06-24 21:06:45 -0700

    Update mirrors

--- lang_en-shifted_bgm/patch.js
+++ lang_en-shifted_bgm/patch.js
@@ -4,8 +4,7 @@
 	],
 	"id": "lang_en-shifted_bgm",
 	"servers": [
-		"http://thcrap.nmlgc.net/repos/DTM/lang_en-shifted_bgm/",
-		"http://mirrors.thpatch.net/DTM/lang_en-shifted_bgm/"
+		"https://mirrors.thpatch.net/DTM/lang_en-shifted_bgm/"
 	],
 	"title": "English Language pack with shifted music titles in stage"
 }
--- transparent_season_items/patch.js
+++ transparent_season_items/patch.js
@@ -4,8 +4,7 @@
 	],
 	"id": "transparent_season_items",
 	"servers": [
-		"http://thcrap.nmlgc.net/repos/DTM/transparent_season_items/",
-		"http://mirrors.thpatch.net/DTM/transparent_season_items/"
+		"https://mirrors.thpatch.net/DTM/transparent_season_items/"
 	],
 	"title": "Increases the transparency of season items (by Namusan)"
 }
