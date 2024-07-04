c9d10e7021c8897206dd1d75f619ee7e0891ec7c
DTM9025 <DTM9025@outlook.com>
2024-07-04 06:54:56 -0700

    Add current developer of alphes patch

--- alphes/patch.js
+++ alphes/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/DTM/alphes/"
 	],
-	"title": "Alphes Styled Character Portraits (Art by Dairi)"
+	"title": "Alphes Styled Character Portraits (by SuperChrim, art by Dairi)"
 }
--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "DTM9025@outlook.com",
 	"id": "DTM",
 	"patches": {
-		"alphes": "Alphes Styled Character Portraits (Art by Dairi)",
+		"alphes": "Alphes Styled Character Portraits (by SuperChrim, art by Dairi)",
 		"lang_en-shifted_bgm": "English Language pack with shifted music titles in stage",
 		"transparent_season_items": "Increases the transparency of season items (by Namusan)"
 	},
